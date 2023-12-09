591b47b6e2812623e168b0598987378cfdab3f6c
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2023-12-09 13:02:51 +0530

    Completed Mentees Page

--- js/script.js
+++ js/script.js
@@ -12,3 +12,27 @@ function toggleMenu() {
     dropdownMenu.style.animation = "slideIn 0.6s ease-out";
   }
 }
+function changemenumentees() {
+  var mentees = document.getElementById("mentees");
+  var mentors = document.getElementById("mentors");
+  var menteesbutton = document.getElementById("buttonmentees");
+  var mentorsbutton = document.getElementById("buttonmentors");
+  mentors.style.display = "none";
+  menteesbutton.style.backgroundColor = "#1B204A";
+  menteesbutton.style.color = "#fff";
+  mentorsbutton.style.backgroundColor = "#fff";
+  mentorsbutton.style.color = "#1B204A";
+  mentees.style.display = "flex";
+}
+function changemenumentors() {
+  var mentees = document.getElementById("mentees");
+  var mentors = document.getElementById("mentors");
+  var menteesbutton = document.getElementById("buttonmentees");
+  var mentorsbutton = document.getElementById("buttonmentors");
+  mentees.style.display = "none";
+  mentors.style.display = "flex";
+  mentorsbutton.style.backgroundColor = "#1B204A";
+  mentorsbutton.style.color = "#fff";
+  menteesbutton.style.backgroundColor = "#fff";
+  menteesbutton.style.color = "#1B204A";
+}
